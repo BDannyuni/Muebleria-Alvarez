953f086e793a8d028e9eafe50d62b97cfcbfad07
Bdannyuni <brandon.alvarezhrnd@uanl.edu.mx>
2024-05-28 16:47:28 -0600

    agregue lo de poco stock

--- pages/resumen.php
+++ pages/resumen.php
@@ -207,7 +207,7 @@ $result = $conn->query($sql);
                         </a>
                     </li>
                     <li class="mega-menu mega-menu-sm">
-                        <a href="#" aria-expanded="false">
+                        <a href="pocostock.php" aria-expanded="false">
                             <i class="ti-layers"></i><span class="nav-text">Productos con Poco Stock</span>
                         </a>
                     </li>
